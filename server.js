556b50aa87741770122533dc15e83546375d2093
Disha <bidisha2125@gmail.com>
2023-07-19 12:05:35 +0530

    Done with basic crud operation

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const connectDB = require('./server/database/connection')
+
 const app = express();
 
 dotenv.config();
@@ -11,6 +13,9 @@ const PORT = process.env.PORT || 5000;
 
 //log requests
 app.use(morgan("tiny"));
+
+//mongodb connection
+connectDB();
 
 //parse request to body parser
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,16 +29,8 @@ app.use('/css', express.static(path.resolve(__dirname,"assets/css")));
 app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
 app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
 
-app.get('/', (req, res) => {
-    res.render("index");
-})
-app.get("/add-user", (req, res) => {
-  res.render("add_user");
-});
-
-app.get("/update-user", (req, res) => {
-  res.render("update_user");
-});
+//load routers
+app.use('/', require('./server/routes/router'));
 
 app.listen (PORT, () => {
     console.log(`listening on http://localhost:${PORT}`);
