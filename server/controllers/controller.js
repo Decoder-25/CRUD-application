7edaeb1979cd45d942fb3b403f5dc815bb928a35
Disha <bidisha2125@gmail.com>
2023-07-19 12:05:08 +0530

    Added the user GET PUT POST DELETE method

---
+++ server/controllers/controller.js
@@ -0,0 +1,81 @@
+var Userdb = require('../models/model');
+
+// create and save new user
+exports.create = (req,res) => {
+    //validate request
+    if(!req.body){
+        res.status(400).send({message: 'Content can not be empty'});
+        return;
+    }
+
+    //new user
+    const user = new Userdb({
+        name:req.body.name,
+        email:req.body.email,
+        gender:req.body.gender,
+        status:req.body.status
+    })
+
+    //save user in the database
+    user
+        .save(user)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err =>{
+            res.status(500).send({message: err.message || "Some error occured while creating a create operation"});
+        })
+}
+
+//retrieve and return all users/retrive and return a single user
+exports.find = (req,res) => {
+    Userdb.find()
+    .then(user => {
+        res.send(user);
+    }) 
+    .catch(err => {
+        res.status(500).send({message: err.message || "Error occured while retrieving user information"});
+    })
+}
+
+//update a new identified user by user id
+exports.update = (req, res) => {
+    if (!req.body) {
+      res.status(400).send({ message: "Data to update can not be empty" });
+      return;
+    }
+
+    const id = req.params.id;
+    Userdb.findByIdAndUpdate(id, req.body, {useFindAndModify: false})
+    .then(data => {
+        if(!data){
+            res.status(404).send({message:"Cannot Update user with ${id}. User not found."});
+        }else{
+            res.send(data);
+        }
+    })
+    .catch(err=>{
+        res.status(500).send({message:"Error update user information."});
+    })
+}
+
+//delete a user by user id
+exports.delete = (req, res) => {
+    const id = req.params.id;
+
+    Userdb.findByIdAndDelete(id)
+      .then((data) => {
+        if (!data) {
+          res
+            .status(404)
+            .send({
+              message: "Cannot Delete user with ${id}. User not found.",
+            });
+        } else {
+          res.send({message:"User deleted successfully"});
+        }
+      })
+      .catch((err) => {
+        res.status(500).send({ message: "Could not delete User with id="+id});
+      });
+}
